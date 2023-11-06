0fb7b398b32776d1cf6b6a36b2f1c65ba415197d
Thomas <G00415457@atu.ie>
2023-11-06 17:45:01 +0000

    got arraylist working

--- src/main/java/ie/atu/ArrayListDemo.java
+++ src/main/java/ie/atu/ArrayListDemo.java
@@ -9,6 +9,7 @@ public class ArrayListDemo {
     public static void main (String[] args)
     {
         ArrayList<Integer> arrayList = new ArrayList<>();
+
         Scanner scanner = new Scanner(System.in);
 
         arrayList.add(100);
@@ -22,13 +23,15 @@ public class ArrayListDemo {
         arrayList.add(900);
         arrayList.add(1000);
 
-        for(int i = 0; i <= 9; i++)
+        System.out.println("Please enter a new value: ");
+        int x = scanner.nextInt();
+
+        arrayList.add(x);
+
+        for(int i = 0; i <= 10; i++)
         {
-            System.out.println("Element at index "+ i +":" + arrayList.get(0) + i);
+            System.out.println("Element at index "+ i +":" + arrayList.get(i));
         }
 
-
-        System.out.println("Please enter a new value: ");
-        arrayList.set(10, scanner.nextInt());
     }
 }
